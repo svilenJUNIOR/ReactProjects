8a0da6224a413aa2b6e39a803e3355b22ed69a7f
svilenJUNIOR <svilenborianov@gmail.com>
2024-10-05 11:24:27 +0300

    Player name / symbol area

--- TicTacToe/src/App.jsx
+++ TicTacToe/src/App.jsx
@@ -1,6 +1,23 @@
 function App() {
   return (
-    <p>coming soon</p>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+
+        </ol>
+      
+      </div>
+    </main>
   )
 }
 
