368d66388ab17e249431e94c4747ad608b91a53c
svilenJUNIOR <svilenborianov@gmail.com>
2024-10-05 12:48:22 +0300

    Name now changes

--- TicTacToe/src/components/PlayerInfo.jsx
+++ TicTacToe/src/components/PlayerInfo.jsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 
 export function RenderPlayerInfo({name, symbol}) {
     const [isEditing, setIsEditing] = useState(false)
+    const [playerName, setPlayerName] = useState(name)
 
   return (
     <li>
       <span>
-        {!isEditing ? <span className="player-name">{name}</span> : <input type="text" required value={name}/>}
+        {
+        !isEditing 
+        ? <span className="player-name">{playerName}</span> 
+        : <input type="text" required value={playerName} onChange={(event) => setPlayerName(event.target.value)}/>
+        }
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={() => setIsEditing(!isEditing)}>{isEditing ? "Save" : "Edit"}</button>
+      <button onClick={() => setIsEditing(edit => !edit)}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
