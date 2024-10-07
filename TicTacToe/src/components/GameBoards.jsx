325652cd98fc89ab0df91a8dfbe544c11f489c39
svilenJUNIOR <svilenborianov@gmail.com>
2024-10-07 10:42:53 +0300

    Symbols now appear when clicked on board

--- TicTacToe/src/components/GameBoards.jsx
+++ TicTacToe/src/components/GameBoards.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,25 @@ const initialBoard = [
 ];
 
 export function GameBoard(){
+    const [board, setBoard] = useState(initialBoard);
+
+    function handleSquareClick (rowIndex, colIndex, selectedSymbol) {
+
+        setBoard((prevGameBoard) =>{
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = "X";
+            return updatedGameBoard;
+        })
+    };
+
     return (
         <ol id="game-board">
-            {initialBoard.map((row, rowIndex) => 
+            {board.map((row, rowIndex) => 
             <li key={rowIndex}>
                 <ol>
                     {row.map((symbol, colIndex) => 
                     <li key={colIndex}>
-                        <button>{symbol}</button>
+                        <button onClick={() => handleSquareClick(rowIndex, colIndex)}>{symbol}</button>
                     </li>)}
                 </ol>
             </li>)}
